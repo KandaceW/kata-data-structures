83583179c9d57fddcfc6a7278090b0513c5191e6
student <kiwdev1984@icloud.com>
2019-05-29 13:54:37 +1200

    Passed another test

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,9 @@
-function getPropTypes (obj) {
+function getPropTypes(obj) {
+  var values = Object.values(obj);
+  var newArr = [];
+  for (i = 0; i < values.length; i++) {
+    newArr.push(typeof values[i]);
+  }
+  return newArr;
 }
+module.exports = getPropTypes;
