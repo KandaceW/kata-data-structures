eea0aa0c1b87c926827b6a3dd797644b0c41338c
Reuben Osborne <10952926+reubenosborne@users.noreply.github.com>
2019-05-29 16:10:21 +1200

    Add find.js

--- find.js
+++ find.js
@@ -1,5 +1,20 @@
 module.exports = find
 
-function find (arr, searchDetails) {
+function find(arr, searchDetails) {
+  let searchKey = Object.keys(searchDetails)[0]
 
+
+  for (let i = 0; i < arr.length; i++) {
+
+    let item = arr[i]
+    let itemValues = Object.values(item)
+    let searchParameter = Object.values(searchDetails)[0]
+
+    if (item[searchKey] == searchDetails[searchKey]) {
+      return item
+    }
+
+
+
+  }
 }
