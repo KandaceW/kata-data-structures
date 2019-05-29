7d4c8604a6f92e3d1a6a37fc626a6b32565cb93f
serenade-akaata <49008908+serenade-akaata@users.noreply.github.com>
2019-05-29 15:52:06 +1200

    adding matrix

--- matrix.js
+++ matrix.js
@@ -1,5 +1,20 @@
+module.exports = {
+  getMatrix:getMatrix,
+  updateMatrix: updateMatrix
+}
+
+
 function getMatrix (n) {
+  console.log(n)
+var matrix = [];
+
+
 }
 
 function updateMatrix (matrix, coords, value) {
+console.log(matrix)
+console.log(coords)
+console.log(value)
+
+
 }
