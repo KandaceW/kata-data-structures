c323512b263f892dfdd52e5cd524fe0badc87c20
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-05-29 15:32:53 +1200

    this is a commit

--- where.js
+++ where.js
@@ -1,2 +1,17 @@
-function where (arr, searchDetails) {
+module.exports = where;
+
+function where(arr, searchDetails) {
+  // console.log(searchDetails);
+  let newArray = [];
+  let keys = Object.keys(searchDetails);
+  for (let i = 0; i < arr.length; i++) {
+    var matchedAll = true;
+    for (let j = 0; j < keys.length; j++) {
+      var key = keys[j];
+      console.log(key);
+      matchedAll = matchedAll && arr[i][key] === searchDetails[key];
+    }
+    if (matchedAll) newArray.push(arr[i]);
+  }
+  return newArray;
 }
