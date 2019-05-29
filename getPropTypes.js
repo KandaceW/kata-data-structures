57e8d8127ad58333d3da95e28d9361daa0a1d0f7
Hanh <47132194+hanh008@users.noreply.github.com>
2019-05-29 13:59:07 +1200

    add another test

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,12 @@
-function getPropTypes (obj) {
+module.exports = getPropTypes
+
+function getPropTypes(obj) {
+  var newObj = Object.values(obj)
+
+  var count = []
+
+  for (var i = 0; i < newObj.length; i++) {
+    count.push(typeof newObj[i])
+  }
+  return count
 }
