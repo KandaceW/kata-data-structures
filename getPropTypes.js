661fc0c7e0e7efdfe8986b999f3ae800c0a046ec
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-05-29 13:44:56 +1200

    this is a commit

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,14 @@
-function getPropTypes (obj) {
+module.exports = getPropTypes;
+
+function getPropTypes(obj) {
+  // let myArray = []
+  // Object.keys(obj).map(function(item) {
+  //   myArray.push (typeof obj[item])
+  //   });
+  //   return myArray
+
+  let myArr = Object.keys(obj).map(function(item) {
+    return typeof obj[item];
+  });
+  return myArr;
 }
--- tests/getPropTypes.test.js
+++ tests/getPropTypes.test.js
@@ -1,20 +1,21 @@
-const getPropTypes = require('../getPropTypes')
+const getPropTypes = require("../getPropTypes");
 
-test('getPropTypes returns the types of object properties', function () {
+test("getPropTypes returns the types of object properties", function() {
   // Arrange
   const objWithDifferentTypes = {
-    a: 'c',
+    a: "c",
     b: 2,
-    c: function () { return 'hello' },
+    c: function() {
+      return "hello";
+    },
     d: false,
     e: {}
-  }
-  const expected = ['string', 'number', 'function', 'boolean', 'object']
+  };
+  const expected = ["string", "number", "function", "boolean", "object"];
 
   // Act
-  const actual = getPropTypes(objWithDifferentTypes)
+  const actual = getPropTypes(objWithDifferentTypes);
 
   // Assert
-  expect(actual).toEqual(expected)
-})
-
+  expect(actual).toEqual(expected);
+});
