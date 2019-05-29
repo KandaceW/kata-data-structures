7d2a437704484e95777eb57533942f5ba7a1b3ff
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-29 13:40:25 +1200

    this is a commit

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,11 @@
+module.exports = getPropTypes 
+
 function getPropTypes (obj) {
+    console.log (obj)
+    var props = Object.values(obj)
+    var newArr = []
+    for(var i = 0; i < props.length; i++){
+        newArr.push(typeof props[i])
+    }
+    return newArr
 }
