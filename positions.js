c246a642a35eddee8ae78668ff9c7bc95b2c0983
Hanh <47132194+hanh008@users.noreply.github.com>
2019-05-29 14:21:23 +1200

    add another test

--- positions.js
+++ positions.js
@@ -1,5 +1,14 @@
+module.exports = {
+  getFirst: getFirst,
+  getLast: getLast,
+}
+
 function getFirst (arr) {
+  console.log(arr)
+  return arr[0]
 }
 
 function getLast (arr) {
+  return arr[arr.length - 1]
 }
+
