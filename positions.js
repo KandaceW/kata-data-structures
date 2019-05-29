dc0892e27e4830c0480b8ed730a9bc1116668295
student <kiwdev1984@icloud.com>
2019-05-29 14:14:38 +1200

    Passed another test

--- positions.js
+++ positions.js
@@ -1,14 +1,18 @@
-function getFirst (arr) {
+function getFirst(arr) {
+  return arr[0];
 }
 
 //'positions.getFirst gets the first item in an array
 
-function getLast (arr) {
+function getLast(arr) {
+  var i = arr.length;
+  // console.log(arr.length)
+  return arr[i - 1];
 }
 
 //positions.getLast gets the last item in a small array
 
 module.exports = {
   getFirst: getFirst,
-  getLast: getLast,
-}
+  getLast: getLast
+};
