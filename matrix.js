a8b4ce21ef9bec9e56389eab39d2e91f5253e198
Hanh <47132194+hanh008@users.noreply.github.com>
2019-05-29 15:00:20 +1200

    add another test

--- matrix.js
+++ matrix.js
@@ -1,5 +1,30 @@
-function getMatrix (n) {
+module.exports = {
+  getMatrix: getMatrix,
+  updateMatrix: updateMatrix,
 }
 
-function updateMatrix (matrix, coords, value) {
+function getMatrix(n) {
+  var cells = []
+
+
+  for (var i = 0; i < n; i++) {
+    cells[i] = [];
+    for (var j = 0; j < n; j++) {
+      cells[i][j] = 0;
+    }
+  }
+  return cells
+}
+
+
+
+function updateMatrix(matrix, coords, value) {
+  // matrix[coords[0]][coords[1]] = value
+  // return matrix
+  
+  var x = coords[0]
+  var y = coords[1]
+  matrix[x][y] = value
+  return matrix
+
 }
