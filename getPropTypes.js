4c7eec14e4840e7d26720d6e2e6bcc19bd78e8bb
Danbez13 <daniel.bez13@gmail.com>
2019-05-29 15:09:49 +1200

    getPropTypesDone

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,6 +1,10 @@
 module.exports = getPropTypes
 
 function getPropTypes (obj) {
-   var arr = Object.keys(obj)
-   
+   const keys = Object.values(obj)
+   var arr = []
+   for(var i = 0; i < keys.length; i++){
+      arr.push(typeof keys[i])
+   }
+   return arr
 }
