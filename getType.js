be059ac409ac4f4bb45bbdf8399712f5040b4493
dan <mikewipa@gmail.com>
2019-05-29 13:10:43 +1200

    add kata data structures tests

--- getType.js
+++ getType.js
@@ -1,2 +1,8 @@
-function getType (thing) {
+module.exports = getType
+
+function getType(thing) {
+  var result = typeof thing
+  return result
 }
+
+
--- getValue.js
+++ getValue.js
@@ -1,2 +1,7 @@
-function getValue (obj, key) {
+module.exports = getValue
+
+function getValue(obj, key) {
+  console.log(obj)
+
+  return obj[key]
 }
