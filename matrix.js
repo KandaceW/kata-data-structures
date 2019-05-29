c228eb2ba59da937378bdfb70977bde51ddb1824
student <kiwdev1984@icloud.com>
2019-05-29 14:43:29 +1200

    Passed another test

--- matrix.js
+++ matrix.js
@@ -1,5 +1,23 @@
-function getMatrix (n) {
-}
+module.exports = {
+  getMatrix: getMatrix,
+  updateMatrix: updateMatrix
+};
 
-function updateMatrix (matrix, coords, value) {
+function getMatrix(n) {
+  //console.log(n)
+  var newArr = [];
+  var completeArr = [];
+
+  for (i = 0; i < n; i++) {
+    newArr.push(0);
+  }
+
+  for (i = 0; i < n; i++) {
+    completeArr.push(newArr);
+  }
+  return completeArr;
 }
+//create an array of arrays that is n x n
+// n is an intiger
+
+function updateMatrix(matrix, coords, value) {}
