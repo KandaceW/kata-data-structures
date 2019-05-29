bdd9ca6dc34f0b946aa5effd4b89cf3b74f1dd59
Hanh <47132194+hanh008@users.noreply.github.com>
2019-05-29 15:33:25 +1200

    add another test

--- find.js
+++ find.js
@@ -1,2 +1,14 @@
-function find (arr, searchDetails) {
+module.exports = find
+
+
+function find(arr, searchDetails) {
+  var searchKey = Object.keys(searchDetails)[0]
+  var searchValue = Object.values(searchDetails)[0]
+
+  for (var i = 0; i < arr.length; i++) {
+    var item = arr[i]
+    if (item[searchKey] == searchValue) {
+      return item
+    }
+  }
 }
