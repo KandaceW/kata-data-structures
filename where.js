d38bdb1045a9fdb405b51814ebdd3657ac3aefd7
Ben Sherwood <48200039+ben-sherwood@users.noreply.github.com>
2019-05-29 15:50:53 +1200

    Final commit

--- where.js
+++ where.js
@@ -1,17 +1,36 @@
 function where(arr, searchDetails) {
+  // var values = Object.values(searchDetails)
+  // var keys = Object.keys(searchDetails)
+  // var result = []
+
+
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (values.length < 2) {
+  //     if (arr[i][keys[0]] == values[0]) {
+  //       result.push(arr[i])
+
+  //     }
+  //   } else if (arr[i][keys[0]] == values[0] && arr[i][keys[1]] == values[1]) {
+  //     result.push(arr[i]) 
+  //   }
+  // }
+  // return result
+
+
   var values = Object.values(searchDetails)
   var keys = Object.keys(searchDetails)
   var result = []
-
-
+  var isMatch = false
   for (var i = 0; i < arr.length; i++) {
-    if (values.length < 2) {
-      if (arr[i][keys[0]] == values[0]) {
-        result.push(arr[i])
-
+    for (var j = 0; j < values.length; j++) {
+      if (arr[i][keys[j]] == values[j]) {
+        isMatch = true;
+      } else {
+        isMatch = false;
       }
-    } else if (arr[i][keys[0]] == values[0] && arr[i][keys[1]] == values[1]) {
-      result.push(arr[i]) 
+    }
+    if (isMatch) {
+      result.push(arr[i])
     }
   }
   return result
