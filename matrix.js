865f4a6dd1f508300d5b17849a8c83a6efe6d308
OliverPetri <49376686+OliverPetri@users.noreply.github.com>
2019-05-29 14:43:37 +1200

    Commit 6

--- matrix.js
+++ matrix.js
@@ -1,5 +1,36 @@
 function getMatrix (n) {
+  
+  var matrix = []
+
+  var arr = []
+  for (var i=0; i<n; i++){
+    arr[i] = 0
+    
+
+  }
+
+for (var i=0; i<n; i++) {
+  matrix[i] = arr
+}return matrix
 }
 
+
 function updateMatrix (matrix, coords, value) {
+
+var row = coords[0]
+var col = coords[1]
+matrix[row][col] = value
+return matrix
+
+  console.log (matrix)
+  console.log (coords)
+  console.log (value)
+
+}
+
+module.exports = {
+
+  getMatrix: getMatrix,
+  updateMatrix: updateMatrix,
+
 }
