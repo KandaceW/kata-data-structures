ddebb416491babda535bf637d8d359b8872408fb
Daniel Bridewell <48977884+daniel-bridewell@users.noreply.github.com>
2019-05-29 12:35:49 +1200

    Dan commit

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,11 @@
 function getPropTypes (obj) {
+  var props = Object.values(obj)
+  var arr = []
+  for (var i=0; i<props.length; i++){
+    arr.push(typeof props[i])
+  }
+  return arr
 }
+
+
+module.exports = getPropTypes
