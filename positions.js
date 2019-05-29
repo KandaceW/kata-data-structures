07c4815157239655c9beecb9fb9e20e1ee3fe9c8
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-29 14:01:12 +1200

    this is a commit

--- positions.js
+++ positions.js
@@ -1,5 +1,15 @@
+
+module.exports = {
+    getFirst: getFirst,
+    getLast: getLast
+}
 function getFirst (arr) {
+    return arr[0]
+    console.log(arr);
+    
 }
 
 function getLast (arr) {
+   return arr.slice(-1)[0]
+
 }
