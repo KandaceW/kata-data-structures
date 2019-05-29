b79649e09dc070aca657654692489880cf726304
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-05-29 15:26:51 +1200

    commit 7

--- find.js
+++ find.js
@@ -1,2 +1,29 @@
 function find (arr, searchDetails) {
+
+var searchKey = Object.keys(searchDetails)[0]
+var searchValue = Object.values(searchDetails)[0]
+console.log('Search key',searchKey)
+console.log('Search value',searchValue)
+
+// if(searchDetails = Object.values(arr[2])) {console.log(true)}
+
+// console.log(arr[2])
+
+    for (i=0; i<arr.length; i++){
+      var item = arr[i]
+      console.log('Testing ', item)
+      console.log(item[searchKey])
+      if(item[searchKey] == searchValue) {
+        console.log('MATCH!')
+        return item
+      }
+
+      // does this item in the array have the same value as seartchDetails
+    // if (test is true) {
+      // if(searchDetails = Object.values(arr[2]))
+    // return arr[i]
+    // }
+  }
+  
 }
+module.exports = find
