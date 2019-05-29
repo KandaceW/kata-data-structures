74c1a69ae03e16420bbf37c04266aa1b01c161e7
student <kiwdev1984@icloud.com>
2019-05-29 15:05:46 +1200

    Passed another test

--- matrix.js
+++ matrix.js
@@ -20,4 +20,19 @@ function getMatrix(n) {
 //create an array of arrays that is n x n
 // n is an intiger
 
-function updateMatrix(matrix, coords, value) {}
+function updateMatrix(matrix, coords, value) {
+  // console.log(matrix)
+  // console.log(coords)
+  // console.log(value)
+
+  var pos1 = coords[0];
+  var pos2 = coords[1];
+
+  console.log(pos1);
+  console.log(pos2);
+
+  matrix[pos1][pos2] = value;
+
+  return matrix;
+}
+//can change the value at specified coordinates
