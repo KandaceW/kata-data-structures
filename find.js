832331d8b91844323416273e318f2735633750d5
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-05-29 15:15:17 +1200

    this is a commit

--- find.js
+++ find.js
@@ -1,2 +1,10 @@
-function find (arr, searchDetails) {
+module.exports = find;
+
+function find(arr, searchDetails) {
+  let key = Object.keys(searchDetails);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][key] === searchDetails[key]) {
+      return arr[i];
+    }
+  }
 }
