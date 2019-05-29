bac4654ce635e1a2ea32989a67c4219475a6fbe9
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-05-29 15:49:35 +1200

    Passed another test

--- find.js
+++ find.js
@@ -1,6 +1,11 @@
-module.exports = find
+module.exports = find;
 
-function find (arr, searchDetails) {
-  console.log(arr)
-  console.log(searchDetails)
+function find(arr, searchDetails) {
+  console.log(arr);
+  console.log(searchDetails);
 }
+//object.keys
+//object.values
+//item = arr[i]
+// for loop and if statement for a test
+//finds the first matching object in the array
